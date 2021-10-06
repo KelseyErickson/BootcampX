a855f829359b2ff3e4329a24023d6a0e3c562a71
Kelsey Erickson <kerickson@gmx.com>
2021-10-06 20:58:03 +0000

    Paramerterize queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`${cohortName}`]
 
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
@@ -14,9 +16,9 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
